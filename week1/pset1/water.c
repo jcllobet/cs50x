60a29ee8a0ec6fa3911da990e406c368834c3b2b
Jan Carbonell <jcllobet@gmail.com>
2017-08-31 01:08:24 +0200

    how many bottles?

---
+++ week1/pset1/water.c
@@ -0,0 +1,9 @@
+#include <cs50.h>
+#include <stdio.h>
+    
+int main(void){ 
+    printf("Minutes:");
+    int minutes = get_int();
+    int bottles = minutes * 12;
+    printf("Bottles: %i\n", bottles);
+}
