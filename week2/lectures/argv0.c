ad0bbf7c8cd478a5b98a549ec4d4a146b1c83b6b
Jan Carbonell <jcllobet@gmail.com>
2017-09-07 16:18:18 +0200

    int main(int argc, string argv[])

---
+++ week2/lectures/argv0.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc == 2){
+    printf("hello %s\n", argv[1]);
+    
+//automatically the computer is creating an argv[0]        
+    }else{
+        printf("hello, world\n");
+    }
+}
