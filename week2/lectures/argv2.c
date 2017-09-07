2f4a799cabbd6ebd4822586972e5bbb91a3d3d44
Jan Carbonell <jcllobet@gmail.com>
2017-09-07 16:19:32 +0200

    [i] [j] array indexing and printf

---
+++ week2/lectures/argv2.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    //iterate over strings in argv
+    for (int i =0; i < argc; i++)
+    {
+        // iterate over characters over current string
+        for (int j = 0, n = strlen(argv[i]); j < n; j++)
+        {
+        // print the j'th character in i'th string
+        printf("%c\n", argv[i][j]);
+        }
+    }
+}
