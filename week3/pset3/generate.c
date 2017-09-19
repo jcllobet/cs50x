34a170e4ae49b573203fdc756631c4b25eed3dff
Jan Carbonell <jcllobet@gmail.com>
2017-09-19 20:05:06 +0200

    Update generate.c

--- week3/pset3/generate.c
+++ week3/pset3/generate.c
@@ -1,212 +1,52 @@
 /**
- * fifteen.c
+ * generate.c
  *
- * Implements Game of Fifteen (generalized to d x d).
+ * Generates pseudorandom numbers in [0,MAX), one per line.
  *
- * Usage: fifteen d
+ * Usage: generate n [s]
  *
- * whereby the board's dimensions are to be d x d,
- * where d must be in [DIM_MIN,DIM_MAX]
- *
- * Note that usleep is obsolete, but it offers more granularity than
- * sleep and is simpler to use than nanosleep; `man usleep` for more.
+ * where n is number of pseudorandom numbers to print
+ * and s is an optional seed
  */
  
-#define _XOPEN_SOURCE 500
+#define _XOPEN_SOURCE
 
 #include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <time.h>
 
-// constants
-#define DIM_MIN 3
-#define DIM_MAX 9
-
-// board
-int board[DIM_MAX][DIM_MAX];
-
-// dimensions
-int d;
-
-// prototypes
-void clear(void);
-void greet(void);
-void init(void);
-void draw(void);
-bool move(int tile);
-bool won(void);
+// upper limit on range of integers that can be generated
+#define LIMIT 65536
 
 int main(int argc, string argv[])
 {
-    // ensure proper usage
-    if (argc != 2)
+    // input has to be either generate n or generate n seed, thus argc 2 && 3 are required.
+    if (argc != 2 && argc != 3)
     {
-        printf("Usage: fifteen d\n");
+        printf("Usage: ./generate n [s]\n");
         return 1;
     }
 
-    // ensure valid dimensions
-    d = atoi(argv[1]);
-    if (d < DIM_MIN || d > DIM_MAX)
+    // Transforms the first string of the array into n(int)
+    int n = atoi(argv[1]);
+
+    // if seed is given, initialize it with that number, otherwise, w/ time(NULL)
+    if (argc == 3)
     {
-        printf("Board must be between %i x %i and %i x %i, inclusive.\n",
-            DIM_MIN, DIM_MIN, DIM_MAX, DIM_MAX);
-        return 2;
+        srand48((long) atoi(argv[2]));
+    }
+    else
+    {
+        srand48((long) time(NULL));
     }
 
-    // open log
-    FILE *file = fopen("log.txt", "w");
-    if (file == NULL)
+    // For every integer in range of the number of numbers, print an int[drand4()*LIMIT]
+    for (int i = 0; i < n; i++)
     {
-        return 3;
+        printf("%i\n", (int) (drand48() * LIMIT));
     }
-
-    // greet user with instructions
-    greet();
-
-    // initialize the board
-    init();
-
-    // accept moves until game is won
-    while (true)
-    {
-        // clear the screen
-        clear();
-
-        // draw the current state of the board
-        draw();
-
-        // log the current state of the board (for testing)
-        for (int i = 0; i < d; i++)
-        {
-            for (int j = 0; j < d; j++)
-            {
-                fprintf(file, "%i", board[i][j]);
-                if (j < d - 1)
-                {
-                    fprintf(file, "|");
-                }
-            }
-            fprintf(file, "\n");
-        }
-        fflush(file);
-
-        // check for win
-        if (won())
-        {
-            printf("ftw!\n");
-            break;
-        }
-
-        // prompt for move
-        printf("Tile to move: ");
-        int tile = get_int();
-        
-        // quit if user inputs 0 (for testing)
-        if (tile == 0)
-        {
-            break;
-        }
-
-        // log move (for testing)
-        fprintf(file, "%i\n", tile);
-        fflush(file);
-
-        // move if possible, else report illegality
-        if (!move(tile))
-        {
-            printf("\nIllegal move.\n");
-            usleep(500000);
-        }
-
-        // sleep thread for animation's sake
-        usleep(500000);
-    }
-    
-    // close log
-    fclose(file);
 
     // success
     return 0;
-}
-
-/**
- * Clears screen using ANSI escape sequences.
- */
-void clear(void)
-{
-    printf("\033[2J");
-    printf("\033[%d;%dH", 0, 0);
-}
-
-/**
- * Greets player.
- */
-void greet(void)
-{
-    clear();
-    printf("WELCOME TO GAME OF FIFTEEN\n");
-    usleep(2000000);
-}
-
-/**
- * Initializes the game's board with tiles numbered 1 through d*d - 1
- * (i.e., fills 2D array with values but does not actually print them).  
- */
-void init(void)
-{
-
-    int number = 0;
-    for (int n = d - 1; n >= 0; n--){
-        for (int m = d - 1; m >= 0; m--){
-            board [m][n] = number;
-            printf("%i", board[m][n]);
-            number++;
-        }
-    }
-    
-    //If d is even, swap tiles 1 and 2
-    if (d % 2 == 0){
-        int last_row = d - 1;
-        int one_tile = d - 2;
-        int second_tile = d - 3;
-        
-        int temporal = board[last_row][one_tile];
-        board[last_row][one_tile] = board[last_row][second_tile];
-        board[last_row][second_tile] = temporal;
-    }
-}
-
-/**
- * Prints the board in its current state.
- */
-void draw(void)
-{
-    for(int i = 0; i < d; i++) {
-        for (int j = 0; j < d; j++){
-            printf("%2i", board[i][j]);
-        printf("\n");
-        }
-    }
-}
-
-/**
- * If tile borders empty space, moves tile and returns true, else
- * returns false. 
- */
-bool move(int tile)
-{
-    // TODO
-    return false;
-}
-
-/**
- * Returns true if game is won (i.e., board is in winning configuration), 
- * else false.
- */
-bool won(void)
-{
-    // TODO
-    return false;
 }
