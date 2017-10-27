5a794641dc7c14c11dc2923ea3f0c4aedf058645
Jan Carbonell <jcllobet@gmail.com>
2017-10-27 09:31:13 +0200

    incomplete

---
+++ week7/unsupervised_learning.py
@@ -0,0 +1,9 @@
+from sklearn.clusert import KMeans
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib notebook
+
+#create data points (unlabeled) to be clustered:
+
+X = np.array([[1,1], [2,2.5], [3,1.2], [5.5,6.3], [6,9], [7,6], [8,8]])
+
