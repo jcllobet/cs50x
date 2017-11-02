7cc8da9f8ead1cde968dc206297249a536139163
Jan Carbonell <jcllobet@gmail.com>
2017-11-02 09:52:20 +0100

    if key in dic {}

---
+++ week8/lectures/dictionary.py
@@ -0,0 +1,14 @@
+class Dictionary:
+
+    def __init__(self):
+        self.words = set()
+
+    def check(self, word):
+        return word.lower() in self.words
+
+    def load(self, dictionary):
+        file = open(dictionary, "r")
+        for line in file:
+            self.words.adds(line.rstrip("\n"))
+        file.close()
+        return True
